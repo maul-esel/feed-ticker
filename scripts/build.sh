2c4bcf545ab7f9435f5feac302eebae5f547e1bf
maul.esel <maul.esel@go4more.de>
2015-09-18 22:50:03 +0200

    build.sh: use clean environment for builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,19 @@
-coffee -c . && jpm xpi
+set -e
+
+echo -e "\n[BUILDING] creating temp repository..."
+rm -rf tmp
+git clone . tmp
+cd tmp
+
+echo -e "\n[BUILDING] compiling coffeescript..."
+coffee -c .
+
+echo -e "\n[BUILDING] packaging XPI..."
+jpm xpi
+
+echo -e "\n[BUILDING] cleaning up..."
+cp *.xpi ..
+cd ..
+rm -rf tmp
+
+echo -e "\n[BUILDING] Done."
